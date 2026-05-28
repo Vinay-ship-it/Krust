ea34cb9cf98a614cb2e6886e76351fc183a24004
Vinay A <vinayananda2@gmail.com>
2026-05-28 14:38:13 +0530

    added some comments for better clarity

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@
 
 use core::panic::PanicInfo;
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // disabling mangling to ensure the compiler outputs a function witht the name _start
 pub extern "C" fn _start() -> ! {
     loop{}
 }
 
-#[panic_handler]
+#[panic_handler] //define our panic handler as we are using no_std
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
