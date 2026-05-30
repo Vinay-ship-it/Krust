52cb62a3e5308fd0fb0b747291fcad970af7cd4b
Vinay A <vinayananda2@gmail.com>
2026-05-30 16:02:16 +0530

    added vga buffer file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![no_std]
 #![no_main]
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+static Hello: &[u8] = b"Hello, World!";
+
 #[unsafe(no_mangle)] // disabling mangling to ensure the compiler outputs a function witht the name _start
 pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
     loop{}
 }
 
