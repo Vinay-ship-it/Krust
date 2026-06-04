34659e4f0a81a2fbc8ae5a5298a43f9a2b4b9c74
Vinay A <vinayananda2@gmail.com>
2026-06-05 03:06:28 +0530

    commit changes

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,10 +1,12 @@
+use core::fmt;
+use volatile::Volatile;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
-
 pub enum Color {
     Black = 0,
-    Blue = 1,               
+    Blue = 1,
     Green = 2,
     Cyan = 3,
     Red = 4,
@@ -21,42 +23,36 @@ pub enum Color {
     White = 15,
 }
 
-#[repr(transparent)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
 struct ColorCode(u8);
 
-impl ColorCode{
-    fn new(foreground: Color, background:Color) ->ColorCode{
-        ColorCode((background as u8) <<4 | (foreground as u8))
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
     }
 }
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 struct ScreenChar {
-    ascii_character:u8,
-    color_code:ColorCode,
+    ascii_character: u8,
+    color_code: ColorCode,
 }
 
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
-
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
-
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
-
-
 pub struct Writer {
-    column_position:usize,
+    column_position: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
 }
-
-
-
 
 impl Writer {
     pub fn write_byte(&mut self, byte: u8) {
@@ -67,39 +63,49 @@ impl Writer {
                     self.new_line();
                 }
 
-                let row = BUFFER_HEIGHT -1;
+                let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
-
                 let color_code = self.color_code;
-                self.buffer.chars[row][col]= ScreenChar {
+
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
-                };
-                self.column_position +=1;
+                });
+                self.column_position += 1;
             }
         }
     }
-    fn new_line(&mut self) {/* TODO */}
-}
-impl Writer {
+
+    fn new_line(&mut self) {}
+
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
                 0x20..=0x7e | b'\n' => self.write_byte(byte),
-                _=>self.write_byte(0xfe),
-           }
+                _ => self.write_byte(0xfe),
+            }
         }
     }
 }
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 pub fn print_something() {
+    use core::fmt::Write;
     let mut writer = Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        color_code: ColorCode::new(Color::Pink, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     };
 
     writer.write_byte(b'H');
     writer.write_string("ello ");
+<<<<<<< HEAD
     writer.write_string("Wörld!");
 }
 
@@ -126,3 +132,7 @@ impl Writer //This line is for volatality {
     }
     ...
 }
+=======
+    write!(writer, "the numbers are {} and {}", 42, 1.0/3.0).unwrap();
+}
+>>>>>>> e9ba90b (updated vga_buffer)
