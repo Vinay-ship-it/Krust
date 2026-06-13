04d5c34016e0b93c0355baf044b936f01b96c7b2
vinay-ship-it <vinayananda2@gmail.com>
2026-06-14 02:41:19 +0530

    updated IDT table

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
@@ -67,4 +68,8 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,13 @@ use my_kernel::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World!");
 
+    my_kernel::init();
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
+
+    println!("It did not crash!");
 
     loop {}
 }
